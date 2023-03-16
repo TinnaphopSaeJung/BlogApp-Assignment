dfa5b45cfd771d983238552c5517be3ad77576c8
TinnaphopSaeJung <6210110484@psu.ac.th>
2023-03-17 00:39:22 +0700

    edit homepage

--- blog-app/src/components/header/Header.jsx
+++ blog-app/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className='header'>
         <div className="headerTitles">
-            <span className='headerTitleSm'>React & Node</span>
+            <span className='headerTitleSm'>You & Me</span>
             <span className='headerTitleLg'>Blog</span>
         </div>
         <img 
